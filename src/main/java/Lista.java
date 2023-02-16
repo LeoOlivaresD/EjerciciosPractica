66c09222d170675f33eaf8b843f19f1e52446abc
LeoOlivaresD <cspraword@gmail.com>
2023-02-15 21:15:08 -0300

    Ejercitando orden en listas de objetos

--- src/main/java/Lista.java
+++ src/main/java/Lista.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lista {
@@ -20,6 +21,13 @@ public class Lista {
             System.out.println("animal: " + n);         //su syntaxis consiste en el for seguido por el parenthesis
                                                         // y dentro de él, el tipo de dato y el nombre que se le dara
         }                                               //seguido de "dos puntos" el element al cual se compara, en este caso la lista "animales"
+        System.out.println("Ahora ordenaremos los elementos de la lista alfabéticamente");
+        Collections.sort(animales); //Gracias a la clase "Colections" podemos usar el metodo sort que nos ordenara los elementos de la lista
+        System.out.println("Veamos nuevamente la lista de animales ahora ordenada alfabéticamente");
+
+        for (String n : animales){
+            System.out.println("Animal: " + n);
+        }
 
         System.out.println("Elimination de un elemento en lista\n" +
                 "eliminaremos : pajaro\n" +
@@ -32,5 +40,19 @@ public class Lista {
         for (String n: animales){
             System.out.println("animal: " + n);
         }
+
+        System.out.println("Eliminaremos todos los elementos de una lista con el metodo \"remove all\"\n" +
+                "ejecutando metodo...");
+        animales.removeAll(animales); //metodo para eliminar todo de la lista
+        //animales.clear(); es un metodo que tambien nos permite vaciar una lista
+        System.out.println("metodo ejecutado con exito\n" +
+                "mostrando nuevamente tamaño y elemtos en lista");
+        System.out.println("tamaño lista " + animales.size());
+        System.out.println("listado de elementos en lista: ");
+        for (String n : animales){
+            System.out.println("animal: " +n);
+        }
+        System.out.println("tarea terminada");
     }
+
 }
